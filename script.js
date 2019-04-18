2aca1f2277614ecc2d4997f0a1710a70c1bd1f84
Matt Tallyn <matt@mtallyn.com>
2019-04-18 01:58:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var x = "variable contents";
+console.log(x);
+
+// var myFavColour = prompt("What is your favourite colour?");
+// alert("your favourite colour is " + myFavColour)
+
+var myArray =  new Array('test', 'test2'); 
+console.log(myArray);
