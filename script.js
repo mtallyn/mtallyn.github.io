01074c7ffd70616663184420f513f229719521e0
Matt Tallyn <matt@mtallyn.com>
2019-04-18 02:04:27 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-var x = "variable contents";
-console.log(x);
-
-// var myFavColour = prompt("What is your favourite colour?");
-// alert("your favourite colour is " + myFavColour)
-
-var myArray =  new Array('test', 'test2'); 
-console.log(myArray);
